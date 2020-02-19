d2fb0d267d7dc8fad16d2419d572058e3f95941b
Brighton <brighton@Brightons-MacBook-Pro.local>
2020-02-19 10:37:04 +0200

    first commit

---
+++ precache-manifest.0b4cb8c817ac3bf28aa84e75ad4b8338.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c0a67ff7dfc5ba6a75b1f7ee2021379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "059d48fb5a79b5d36869",
+    "url": "/static/css/2.64a99b10.chunk.css"
+  },
+  {
+    "revision": "be2368127a0251df614c",
+    "url": "/static/css/main.0f6894ef.chunk.css"
+  },
+  {
+    "revision": "059d48fb5a79b5d36869",
+    "url": "/static/js/2.6cd926c6.chunk.js"
+  },
+  {
+    "revision": "d528fbf759715e355b661f5f975dacf7",
+    "url": "/static/js/2.6cd926c6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "be2368127a0251df614c",
+    "url": "/static/js/main.70ffe363.chunk.js"
+  },
+  {
+    "revision": "3fcd0c3d824863bb9305",
+    "url": "/static/js/runtime-main.590f3b6f.js"
+  },
+  {
+    "revision": "bcfcfaf3d2c8ba7e4337ffb178de1616",
+    "url": "/static/media/1.bcfcfaf3.png"
+  },
+  {
+    "revision": "011a0d165f9f3c6085d5328ecac2794a",
+    "url": "/static/media/10.011a0d16.jpg"
+  },
+  {
+    "revision": "b525b5056e0f1f19c6670960045a4720",
+    "url": "/static/media/2.b525b505.png"
+  },
+  {
+    "revision": "8895801bd57ea1286ad9a124be06b0ec",
+    "url": "/static/media/3.8895801b.png"
+  },
+  {
+    "revision": "b8e7e46caa89272626250f50248a9e51",
+    "url": "/static/media/4.b8e7e46c.jpg"
+  },
+  {
+    "revision": "7b988c08cb698ad51d80daf0f1669e17",
+    "url": "/static/media/5.7b988c08.jpg"
+  },
+  {
+    "revision": "5f37559d95a000ca70df4eae98a8d8f0",
+    "url": "/static/media/6.5f37559d.jpg"
+  },
+  {
+    "revision": "2eca1deb07d5c0a9e3987cd58c351b1b",
+    "url": "/static/media/7.2eca1deb.jpg"
+  },
+  {
+    "revision": "1de7ebf038e5f7be134f690cf0172bea",
+    "url": "/static/media/8.1de7ebf0.jpg"
+  },
+  {
+    "revision": "e0bc7cfa7dfe30310fff2f24000db066",
+    "url": "/static/media/8_2.e0bc7cfa.jpg"
+  },
+  {
+    "revision": "1342e9794357696f79d4e8208ae3701b",
+    "url": "/static/media/9.1342e979.jpg"
+  },
+  {
+    "revision": "f61630d75fdf641bef0f0e1eae65d5dd",
+    "url": "/static/media/9_2.f61630d7.jpg"
+  },
+  {
+    "revision": "5439acfb7be0dde0199a5be53faf2b15",
+    "url": "/static/media/bloqq.5439acfb.png"
+  },
+  {
+    "revision": "9325d1a02caff41950c6536bef762cf2",
+    "url": "/static/media/mphifa.9325d1a0.png"
+  },
+  {
+    "revision": "06ccdda7c18e069591ba343898718b60",
+    "url": "/static/media/paddrive.06ccdda7.png"
+  }
+]);
